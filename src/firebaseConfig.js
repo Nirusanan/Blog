3d423f8df33aa70a4623f2828110c3a09144c563
niru9711 <nirusanant97@gmail.com>
2024-03-23 16:01:29 +0530

    config

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -5,12 +5,7 @@ import {
 } from 'firebase/firestore'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCYpnAOI9S2MVP9oWJ4SqxUCWCVMJjsaqc",
-    authDomain: "blog-1f9d9.firebaseapp.com",
-    projectId: "blog-1f9d9",
-    storageBucket: "blog-1f9d9.appspot.com",
-    messagingSenderId: "441882588354",
-    appId: "1:441882588354:web:5e1faf6b0b89050da6775b"
+    //your configuration is here
   };
 
   initializeApp(firebaseConfig)
