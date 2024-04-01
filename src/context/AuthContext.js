c0967d4cacbf234036929d64c634bbb2298f20a9
niru9711 <nirusanant97@gmail.com>
2024-04-01 10:49:17 +0530

    authContext

---
+++ src/context/AuthContext.js
@@ -0,0 +1,29 @@
+// useContext - Autentication (global state)
+import { createContext, useReducer } from "react";
+
+
+// create context
+export const AuthContext = createContext();
+
+//User Reducer
+const userReducer = (state, action) =>{
+    switch (action.type){
+        case 'LOGIN':
+            return {...state, user: action.payload}
+        case 'LOGOUT':
+            return {...state, user: null}
+        default:
+            return state
+    }
+}
+
+export const AuthContextProvider = ({children}) =>{
+
+    const [state, dispatch] = useReducer(userReducer, {user:null})
+
+    return (
+        <AuthContext.Provider value={{...state, dispatch}}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
